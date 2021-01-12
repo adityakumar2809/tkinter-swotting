af9a2a0fa4160822bc78c69aaaa482af7edd84c0
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-12 21:40:18 +0530

    Create a label

--- basics.py
+++ basics.py
@@ -6,8 +6,18 @@ def tkinterSetup():
     return root
 
 
+def createLabel(root):
+    my_label = tkinter.Label(
+        master=root,
+        text='Hello World!'
+    )
+    return my_label
+
+
 def main():
     root = tkinterSetup()
+
+    text_label = createLabel(root)
 
 
 if __name__ == "__main__":
