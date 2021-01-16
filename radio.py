f042e77ae0258a66ee29af7daa629b09f8bbac03
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-16 06:28:43 +0530

    Add feature to position radio buttons

--- radio.py
+++ radio.py
@@ -35,6 +35,9 @@ def main():
         radio_1 = createRadioButton(root, 'Option 1', 1)
         radio_2 = createRadioButton(root, 'Option 2', 2)
         radio_3 = createRadioButton(root, 'Option 3', 3)
+        radio_1.pack()
+        radio_2.pack()
+        radio_3.pack()
         root.mainloop()
 
 
