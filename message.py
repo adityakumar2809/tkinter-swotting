11a1325ecbac6ac5f23cd14304ad8df552563fb2
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-16 06:57:04 +0530

    Add functionality to create a label

--- message.py
+++ message.py
@@ -27,6 +27,14 @@ def createButton(root, text):
     return my_button
 
 
+def createLabel(root, text):
+    my_label = tkinter.Label(
+        master=root,
+        text=text
+    )
+    return my_label
+
+
 def main():
 
     running_status = {
